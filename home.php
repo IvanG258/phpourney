59efe816608e58dc1b2e895979bab80acec0728f
Ivan Guitimela <135559066+IvanG258@users.noreply.github.com>
2025-04-12 22:43:57 +1200

    message

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+require_once'db_conect.php';
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    
+</body>
+</html>
